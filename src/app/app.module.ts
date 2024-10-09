6e7c47e0d5f7425cebbde209f1620b39f00f0053
nahdaicue <nahdaicue@gmail.com>
2024-10-09 04:21:04 -0300

    Cree carpeta FrondEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    SocialComponent,
     BannerComponent,
     AboutComponent,
     ExperienceComponent,
